671cf5fe80f4e51011019c7ef73e70b7e24b2bac
flashniv <flashniv@gmail.com>
2022-03-29 13:04:01 +0300

    Initial Commit

---
+++ simple monitoring/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simple monitoring
+//
+//  Created by Ivan Novobranets on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simple monitoring/simple_monitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_monitoringApp.swift
+//  simple monitoring
+//
+//  Created by Ivan Novobranets on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct simple_monitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
